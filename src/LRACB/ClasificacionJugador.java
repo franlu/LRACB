35bf8a7fc5d546296596c54667f1efb9a8e97efe
franlu <fran.lucena@gmail.com>
2011-01-12 13:03:36 +0100

    All class for proyect

---
+++ src/LRACB/ClasificacionJugador.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package LRACB;
+
+/**
+ *
+ * @author fran
+ */
+public class ClasificacionJugador {
+
+
+    Jornada buscarJornada(int numJornada){
+        Jornada j = new Jornada();
+        return j;
+    }
+    boolean existeJugador(String dniPass){
+
+        boolean existe = true;
+        return existe;
+    }
+}
