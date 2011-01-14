70ae54fb814c9cd56d04bb54dec340a7911f66b8
franlu <fran.lucena@gmail.com>
2011-01-14 13:59:01 +0100

    "Including the head of the class method Jornada"

--- src/LRACB/Jornada.java
+++ src/LRACB/Jornada.java
@@ -5,15 +5,60 @@
 
 package LRACB;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+
 /**
  *
  * @author fran
  */
-public class Jornada {
+class Jornada {
 
     private int numJornada;
     private Fecha fecha;
     private ClasificacionClub cClub;
     private ClasificacionJugador cJugador;
 
+    Jornada(int numJornada, Calendar listaDeFechas){
+    
+        this.numJornada = numJornada;
+    
+    
+    }
+
+    boolean esAnterior(Calendar listaDeFechas){
+
+        return true;
+
+    }
+
+    int obtenerNumero(){
+
+        return this.numJornada;
+    }
+
+    ArrayList obtenerResultados(String nombClub){
+
+    }
+
+    void anotarResultado(){
+    }
+
+    String obtenerNombClubVisi(){}
+
+    void definirClasificacionClub(){}
+
+    ArrayList resultadoJugadores(){}
+
+    private Fecha buscarFecha(){}
+    private ClasificacionClub buscarCClub(String nombClub){}
+
+    ArrayList verPartidoDeJornada(){}
+    ArrayList clasificacionGeneral(){}
+    void ordenarPorPartidosGanados(){}
+    ArrayList rankingAnotadores(){}
+    void ordenaPorRendimiento(){}
+    void anotarResultadoJugador(){}
+    void incluirClasificacion(){}
+    void definirPartido(){}
 }
