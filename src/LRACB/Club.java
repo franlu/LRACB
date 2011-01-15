cf8adc336b07d6c4d6f78f06836c23a2d36135c2
franlu <fran.lucena@gmail.com>
2011-01-15 11:35:40 +0100

    "method head of Club class"

--- src/LRACB/Club.java
+++ src/LRACB/Club.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package LRACB;
 
@@ -10,13 +6,62 @@ import java.util.Map;
 
 /**
  *
- * @author fran
+ * @author Fco Javier Lucena Lucena
  */
-public class Club {
+class Club {
 
     private String nombre;
     private String entrenador;
     private String pabellon;
 
     private Map<String,Jugador> jugadores = new HashMap<String,Jugador>();
+
+    Club(String nombre, String entrenador, String pabellon){
+
+        this.nombre = nombre;
+        this.entrenador = entrenador;
+        this.pabellon = pabellon;
+    }
+
+    String getNombre(){
+
+        return this.nombre;
+
+    }
+
+    String getEntrenador(){
+
+        return this.entrenador;
+
+    }
+
+    String getPabellon(){
+
+        return this.pabellon;
+
+    }
+
+    void setNombre(String nombre){
+
+        this.nombre = nombre;
+
+    }
+
+    void setEntrenador(String entrenador){
+
+        this.entrenador = entrenador;
+
+    }
+
+    void setPabellon(String pabellon){
+
+        this.pabellon = pabellon;
+
+    }
+
+    void crear(String nombre, String entrenador, String pabellon ){}
+    void ficharJugador(Jugador ju){}
+    String obtenerNombre(){}
+    String obtenerPabellon(){}
+    ArrayList jugadoresDeClub(){}
 }
