fb2ebb70b105dcb28c9135b07977e44847b7bafe
franlu <fran.lucena@gmail.com>
2011-01-15 23:23:55 +0100

    "method head of Partido class"

--- src/LRACB/Partido.java
+++ src/LRACB/Partido.java
@@ -9,6 +9,22 @@ package LRACB;
  *
  * @author fran
  */
-public class Partido {
+class Partido {
 
+    private Calendar hora;
+    private String TVemite;
+
+    private ResultadoLocal resultadoLocal;
+    private ResultadoVisitante resultadoVisitante;
+    private Map<int,ResultadoJugador> resultadoJugador = new HashMap<int,ResultadoJugador>();
+
+    void anotarResultado(int puntosLocal, int puntosVisi){}
+    String obtenerNombClubVisi(){}
+    ArrayList resultadoJugadores(){}
+    ArrayList datosDelPartido(){}
+    void anotarResultadoJugador(String dniPas, double minutosJugados, int intentos,
+            int puntosConseguidos){}
+    boolean participaClub(Club cl, Club cv){}
+    public void crear(Club cl, Club cv, Calendar hora, String TVEmite){}
+    
 }
