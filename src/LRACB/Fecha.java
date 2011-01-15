592e5311bea95a81d53f1eb0aadd1119c8f66a72
franlu <fran.lucena@gmail.com>
2011-01-15 23:14:03 +0100

    "method head of Fecha class"

--- src/LRACB/Fecha.java
+++ src/LRACB/Fecha.java
@@ -5,10 +5,36 @@
 
 package LRACB;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author fran
  */
-public class Fecha {
+class Fecha {
 
+    private Calendar dia;
+    private Map<Calendar,Partido> Partidos = new HashMap<Calendar,Partido>();
+
+    void crear(Calendar dia){}
+    boolean esAnterior(Calendar dia){
+
+        return true;
+
+    }
+    String obtenerNombClubVisi(String nombClubLocal){}
+    void anotarResultado(int puntosLocal, int puntosVisi){}
+    ArrayList resultadoJugadores(Calendar dia, String nombClubLocal){}
+    private Partido buscarPartido(String nombClubLocal){}
+    ArrayList partidosDeUnDia(){}
+    void anotarResultadoJugador(String nombClubLocal, String dniPas, double minutosJugados,
+            int intentos, int puntosConseguidos){}
+    // Errata en DCDiseño
+    boolean participaPartido(Club cl, Club cv){}
+    void definirPartido(Club cl, Club cl, Date hora, String TVEmite){}
+    
 }
