f2e8639a857599c3feda135de6f82629cd4ce530
franlu <fran.lucena@gmail.com>
2011-01-15 23:38:11 +0100

    "method head of Jugador class"

--- src/LRACB/Jugador.java
+++ src/LRACB/Jugador.java
@@ -7,8 +7,32 @@ package LRACB;
 
 /**
  *
- * @author fran
+ * @author Francisco Javier Lucena Lucena
  */
-public class Jugador {
+class Jugador {
 
+    private String nombre;
+    private String dniPass;
+    private Calendar fechaNac;
+    private double altura;
+    private String posicion;
+    private double peso;
+    private String nacionalidad;
+    private int numero;
+    
+    private Club club;
+
+    private Map<double,ClasifiacionJugador> clasificacionJugador = new HashMap<>(double,ClasifiacionJugador);
+
+    Jugador(String dniPas, String nombre, Calendar fechaNac, double altura, double peso, String posicion, String nacionalidad , int numero, Club cl){}
+    int obtenerNumero(){}
+    String obtenerNombre(){}
+    String obtenerPosicion(){}
+    ArrayList obtenerDatosJugador(){}
+    String obtenerNombClub(){}
+    private void ordenarPorJornada(){}
+    ArrayList obtenerClasificacion(){}
+    ArrayList obtenerResultados(){}
+    ClasificacionJugador obtenerUltima(){}
+    void incluir(ClasificacionJugador cc){}
 }
