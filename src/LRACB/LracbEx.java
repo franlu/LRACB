6312c93b0285d4dcc1aa1b914cfe258bb8e72afc
franlu <fran.lucena@gmail.com>
2011-01-17 18:11:35 +0100

    "New class: LracbEx"

---
+++ src/LRACB/LracbEx.java
@@ -0,0 +1,25 @@
+/*
+* LracbEx
+*
+* Informacion de la version
+*
+* Fecha
+*
+* Copyright
+*/
+package LRACB;
+
+/**
+ *
+ * @author Fco Javier Lucena Lucena
+ */
+public class LracbEx extends Exception {
+
+    LracbEx(String error) {
+
+        super(error);
+
+    }
+
+}
+
