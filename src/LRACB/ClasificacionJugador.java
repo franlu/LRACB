fee5e3cb11fe099579013ddeb985b805df95c4e8
franlu <fran.lucena@gmail.com>
2011-01-18 23:54:04 +0100

    "Method obtenerDatosJornada of ClasificacionJugador class OK"

--- src/LRACB/ClasificacionJugador.java
+++ src/LRACB/ClasificacionJugador.java
@@ -1,5 +1,7 @@
 package LRACB;
 
+import java.util.ArrayList;
+
 
 
 /**
@@ -16,9 +18,35 @@ class ClasificacionJugador {
     private Jugador jugador;
     private Jornada jornada;
 
+
+    ClasificacionJugador(Jugador jug, Jornada jor, double minutosTotales,
+            int intentosTotales, int puntosTotales){
+
+        this.jugador = jug;
+        this.jornada = jor;
+        this.minutosTotales = minutosTotales;
+        this.intentosTotales = intentosTotales;
+        this.puntosTotales = puntosTotales;
+    
+
+    }
+
     ArrayList obtenerDatosClasificacion(){}
-    ArrayList obtenerDatosJornada(){}
-    ClasificacionJugador(Jugador jug, Jornada jor, double minutosTotales, int intentosTotales, int putnosTotales){}
+
+    ArrayList obtenerDatosJornada(){
+
+        ArrayList datosJornada = new ArrayList();
+        int numJornada = (this.jornada).getNumero();
+        datosJornada.add(numJornada);
+        datosJornada.add(this.puntosTotales);
+        datosJornada.add(this.intentosTotales);
+        datosJornada.add(this.minutosTotales);
+        datosJornada.add(this.rendimiento);
+
+        return datosJornada;
+
+    }
+    
 
     
 }
