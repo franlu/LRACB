14318ff26f5f5f1371d25814bc90aa15b8b026aa
franlu <fran.lucena@gmail.com>
2011-01-18 23:53:26 +0100

    "Method evolucionJugador of Jugador class OK"

--- src/LRACB/Jugador.java
+++ src/LRACB/Jugador.java
@@ -5,7 +5,10 @@
 
 package LRACB;
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -31,7 +34,47 @@ class Jugador {
     Jugador(String dniPas, String nombre, Calendar fechaNac, double altura, 
             double peso, String posicion, String nacionalidad , int numero,
             Club cl){}
-    int obtenerNumero(){}
+    
+    //No aparece en DCD pero si en DCol
+    ArrayList evolucionJugador(){
+    
+        ArrayList resultado = new ArrayList();
+        
+        resultado.add(this.nombre);
+        String nombClub = (this.club).getNombre();
+        resultado.add(nombClub);
+        resultado.add(this.posicion);
+        
+        if ((this.clasificacionJugador).size() == 0) {
+
+            resultado.add("El jugador no ha participado en ninguna jornada");
+
+        }
+        else {
+            // ordenar el map por la clave..
+             //(this.clasificacionJugador).
+             ArrayList datosJornada = new ArrayList();
+             ClasificacionJugador clj;
+             Iterator it = (this.clasificacionJugador).entrySet().iterator();
+
+                while (it.hasNext()) {// Ojo con los casting
+                    Map.Entry e = (Map.Entry)it.next();
+                    clj = (ClasificacionJugador) e.getValue();
+                    datosJornada = clj.obtenerDatosJornada();
+                    resultado.add(datosJornada);
+                }
+
+
+        }
+
+        return resultado;    
+    
+    }
+    int obtenerNumero(){
+
+        return this.numero;
+
+    }
     
     //String obtenerNombre(){}
     String getNombre(){
