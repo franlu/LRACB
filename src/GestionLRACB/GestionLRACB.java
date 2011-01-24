616e36cf54789775ba843e17eb71373f3a3c37ed
franlu <fran.lucena@gmail.com>
2011-01-24 17:10:44 +0100

    "New package GestionLRACB"

---
+++ src/GestionLRACB/GestionLRACB.java
@@ -0,0 +1,19 @@
+
+package GestionLRACB;
+
+import LRACB.LRACB;
+import LRACB.LracbEx;
+/**
+ *
+ * @author Fco Javier Lucena Lucena
+ */
+public class GestionLRACB {
+
+    public static void main(String[] args){
+
+    
+
+
+
+    }
+}
